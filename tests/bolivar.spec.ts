9997b8823ae73f0053a63f9cf8cd1f43d4a92f9a
samirhcc <samir.camargo@segurosbolivar.com>
2025-10-29 13:20:37 -0500

    cd

--- tests/bolivar.spec.ts
+++ tests/bolivar.spec.ts
@@ -11,18 +11,7 @@ test("Test automatizado - Caso 2413", async ({ page, }) => {
   await test.step("Abrir menú de Trámites", async () => {
     await page.getByRole("button", { name: "Trámites " }).click();
   });
-    const indemnizaciones = page.locator("#webComponentHeader").getByText("Indemnizaciones");
-    await indemnizaciones.waitFor({ state: "visible" });
-    await indemnizaciones.click();
-    await page
-      .getByText("SOLICITE SUS INDEMNIZACIONES FÁCIL Y RÁPIDO")
-      .first()
-      .waitFor({ state: "visible" });
+    
   });
 
-  
-  test('Validar botón Acceso a Clientes en Seguros Bolívar', async ({ page }) => {
-  await page.goto('/');
-  const boton = page.getByRole('link', { name: /Acceso a clientes/i });
-  await expect(boton).toBeVisible();
-});
+
