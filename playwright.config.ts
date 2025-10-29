e644fe7d146bc4b9498f00ba49a15c7fceeb7a49
samirhcc <samir.camargo@segurosbolivar.com>
2025-10-29 09:38:35 -0500

    cd

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,6 +18,5 @@ export default defineConfig({
   },
   projects: [
     { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
-    { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
   ],
 });
